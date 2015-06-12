0f4c5830f0f102c1b781fbaafa7d770c8c43d370
AdrianCabrera <adrian@tomorrowscode.com>
2015-06-12 20:29:04 +0100

    crear comentarios

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -20,6 +22,9 @@ router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',			quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
 
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 /* AUTHOR. */
 router.get('/author', function(req, res) {
