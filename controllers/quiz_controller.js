2cc8723818cf58642d0c2b50cb4c1d07e45d2d6c
Adrian Cabrera <11042779+AdrianCabrera@users.noreply.github.com>
2015-06-12 18:36:27 +0100

    update preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,7 +55,7 @@ exports.answer = function(req,res){
 // GET /quizes/new
 exports.new = function(req,res){
 	var quiz = models.Quiz.build( // crea objeto quiz
-		{pregunta: "pregunta", respuesta: "Respuesta"}
+		{pregunta: "pregunta", respuesta: "respuesta"}
 		);
 	res.render('quizes/new',{quiz: quiz,errors: []});
 };
@@ -65,21 +65,81 @@ exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
 	var errors=quiz.validate();
 
-	if(errors){
-		var i=0; 
-		var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
-		for (var prop in errors){ 
-			errores[i++]={message: errors[prop]};
-		}
-		console.log(errores);
-		res.render('quizes/new', {quiz:quiz, errors:errores});
-	}else{
-		// guarda en DB los campos pregunta y respuesta de quizs
-		quiz
-		.save({fields:["pregunta","respuesta"]})
-		.then(function(){
-			//Redirección HTTP (URL relativo) lista de preguntas
-			res.redirect('/quizes');
-		});
-	}
+	// if(errors){
+	// 	var i=0; 
+	// 	var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+	// 	for (var prop in errors){ 
+	// 		errores[i++]={message: errors[prop]};
+	// 	}
+	// 	res.render('quizes/new', {quiz:quiz, errors:errores});
+	// }else{
+	// 	// guarda en DB los campos pregunta y respuesta de quizs
+	// 	quiz
+	// 	.save({fields:["pregunta","respuesta"]})
+	// 	.then(function(){
+	// 		//Redirección HTTP (URL relativo) lista de preguntas
+	// 		res.redirect('/quizes');
+	// 	});
+	// }
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+  }
+  ).catch(function(error){next(error)});
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req,res){
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+
+// PUT /quizes/:id
+exports.update = function(req,res){
+
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+	       		 req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
+	       		 .save({fields: ["pregunta", "respuesta"]})
+	       		 .then( function(){ res.redirect('/quizes')}) 
+	      		}      // res.redirect: Redirección HTTP a lista de preguntas
+	      	}
+	      	).catch(function(error){next(error)});
+
+	// var errors=req.quiz.validate();
+
+	// if(errors){
+	// 	var i=0; 
+	// 	var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+	// 	for (var prop in errors){ 
+	// 		errores[i++]={message: errors[prop]};
+	// 	}
+	// 	res.render('quizes/new', {quiz:req.quiz, errors:errores});
+	// }else{
+	// 	// guarda en DB los campos pregunta y respuesta de quizs
+	// 	req.quiz
+	// 	.save({fields:["pregunta","respuesta"]})
+	// 	.then(function(){
+	// 		//Redirección HTTP (URL relativo) lista de preguntas
+	// 		res.redirect('/quizes');
+	// 	});
+	// }
 };
