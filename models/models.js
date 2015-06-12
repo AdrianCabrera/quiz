d1db74a02bd162c88a35cd4dbe76ba2bec167043
Adrian Cabrera <11042779+AdrianCabrera@users.noreply.github.com>
2015-06-12 12:06:05 +0100

    Fix preguntas

--- models/models.js
+++ models/models.js
@@ -33,9 +33,9 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicaliza la tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 	//sucess(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	Quiz.count().then(function(count){
 		if(count === 0) { // la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 				respuesta: 'Roma'
